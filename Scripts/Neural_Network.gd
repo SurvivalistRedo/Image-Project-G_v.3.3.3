6bfed980c45269190bef35216f1872bd50cea395
SurvivalistRedo <SurvivalistRedo@outlook.com>
2022-12-16 06:33:55 -0800

    added multiplyArray()
    which has happened cuz I was gonna do the sign flip function but that's literally just a subset of the capability of multiplying an array by a single number, since a sign flip is just a multiplication of negative one

--- Scripts/Neural_Network.gd
+++ Scripts/Neural_Network.gd
@@ -198,6 +198,17 @@ func pairArrayAdd(array1,array2):
 				push_error("bruh idk bug in Neural_Network.pairArrayAdd()")
 				get_tree().quit()
 		return array_buffer.duplicate(true)
+func multiplyArray(array,multiplier):
+	var array_buffer = []
+	for n in array.size():
+		if typeof(array[n]) == TYPE_ARRAY:
+			array_buffer.append(multiplyArray(array[n],multiplier).duplicate(true))
+		elif typeof(array[n]) != TYPE_ARRAY:
+			array_buffer.append( array[n] * multiplier )
+		else:
+			push_error("bruh idk bug in Neural_Network.multiplyArray()")
+			get_tree().quit()
+	return array_buffer.duplicate(true)
 func divideArray(array,denominator):
 	var array_buffer = []
 	for n in array.size():
@@ -252,8 +263,6 @@ func NetAddParameterStep(parameterStep,i_steps = 1,record = false):
 			var averagedGradientStep = divideArray(parameterStep[l][n][0].duplicate(true),i_steps)
 			networkArray[l][n][0] = pairArrayAdd(networkArray[l][n][0],averagedGradientStep.duplicate(true))
 			networkArray[l][n][1] = networkArray[l][n][1] + (parameterStep[l][n][1]/i_steps)
-func signFlipNetParametersStep():
-	pass
 func ReverseLastNetParametersRandomStep():
 	if NAS_nPS_H.size()-1 < 0:
 		push_error("NAS_nPS_H.size()-1 < 0")
