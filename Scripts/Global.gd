1372eea68793553dbbf69600bb0094c693fe5f30
SurvivalistRedo <SurvivalistRedo@outlook.com>
2022-12-17 21:16:14 -0800

    added scenes variable, which is an array of scene file paths

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var scenes = ["res://Image Generation/world.tscn","res://Auto Encoding/auto-encoder.tscn","res://Shaders/shadersad.tscn"]
+
 var multRange = 0.01 # 1
 var biasRange = 0.01 # 0.25
 var step_size = 0.01 # 0.01
